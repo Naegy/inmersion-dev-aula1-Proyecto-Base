59dd6784f0453424a56a7373c38a7f60afb7c089
naegy <luferpa1996@gmail.com>
2024-09-17 14:55:31 -0500

    proyecto base

---
+++ main.js
@@ -0,0 +1,53 @@
+//LET/VAR variables tambien alamacena informacion numeros enteros Y STRING
+//let cantidad = 8;  manera base
+//let  permita cambiar el valor
+let cantidad = document.getElementById('cantidad');
+let boton= document.getElementById('generar');
+let contrasena= document.getElementById('contrasena');
+
+const caracteres= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+
+
+//funciones  agreaga actividaddes/acciones  especificas
+function generar (){
+     let numerodigitado= parseInt(cantidad.value);
+    // console.log(numerodigitado);   // verificando/ validador string  transformado a  int   
+
+    let password='';        
+
+    if(numerodigitado<8){
+        alert("Eror Cantidad debe ser mayor a 8 ");
+     
+      }
+      for(let i=0; i<numerodigitado;i++)  // revisar como funciona i++ vs i+1
+        {
+           let caracterandom=caracteres[Math.floor (Math.random() * caracteres.length)];    //lenght toda lalongitud de caracteres
+             // console.log(caracterandom);
+
+                password+=caracterandom;    //+= uso concatena y asigna valor
+
+        }
+              //prueba de que genera password
+       // console.log('Password Generada:'+ password);
+       
+       contrasena.value = password;
+       
+
+}
+
+
+
+//let texto = 'texto';
+//console.log(typeof texto);
+
+
+
+
+
+
+
+
+
+
+
